86851d7cdaee9adf5c8ce8094493dd7b3538d614
Fede dC <fede_delcorro@hotmail.com>
2022-10-26 08:10:16 -0300

    galeria nueva

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-let cad =`
+let cad = /* html */ `
         <div class="contiene-logo">
             <a href="index.html">
                 <img class="logo" src="IMG/Logo_Tu_recital.png" alt="Logo de la página Tu Recital">
@@ -10,7 +10,6 @@ let cad =`
         <nav class="menu">
             <ul class="list">
                 <li><a href="index.html";">Inicio</a></li>
-                <li><a href="recitales.html">Recitales</a></li>
                 <li><a href="contacto.html">Contacto</a></li>
                 <li><a href="acerca-de.html">Acerca de</a></li>
                 <li><a href="galeria.html">Galería</a></li>
@@ -18,9 +17,59 @@ let cad =`
         </nav>
 `
 
+
+let footer =/* html */ `
+
+<div class="logo">
+<a href="index.html">
+  <img class="logo" src="IMG/Logo_Tu_recital.png" alt="Logo de la página Tu Recital" width="150px">
+  <img class="logo oscuro" src="IMG/Logo_Tu_recital_xaOscuro.png" alt="Logo de la página Tu Recital modo oscuro">
+</a>
+</div>
+
+<div>
+<ul class="redes">
+  <li> <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+  <li> <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+  <li> <a href="https://twitter.com/?lang=es" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+  <li> <a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+</ul>
+</div>
+
+<div class="copy">
+<p>Todos los derechos reservados <i class="fa fa-copyright" aria-hidden="true"></i></p>
+<p>Hecho por Esteban M. y Fede dC. ❤</p>
+</div>
+
+`
+
+const imagenes = document.querySelectorAll(".imagen");
+const contieneImg = document.querySelector(".contiene-img");
+const muestraImg = document.querySelector(".show");
+const texto = document.querySelector(".texto");
+const cerrar = document.querySelector(".fa-solid.fa-xmark");
+
 document.getElementById("head").innerHTML=cad;
+document.querySelector(".foot").innerHTML = footer;
 
 
-document.getElementsByClassName("foot").color = "red";
+imagenes.forEach(imagen => {
+  imagen.addEventListener(`click`,()=>{
+    addImagen(imagen.getAttribute("src"), imagen.getAttribute("alt"));
+  })
+})
+
+const addImagen = (src, alt) => {
+  contieneImg.classList.toggle(`centrar`);
+  muestraImg.src = src;
+  texto.innerHTML = alt;
+}
+
+cerrar.addEventListener("click", () => {
+  contieneImg.classList.toggle(`centrar`);
+})
+
+
+
 
 
